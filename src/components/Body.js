f48870b986937dbcb3a845303edaca54280e3087
Nitinr05 <nrahangdale86@gmail.com>
2023-07-07 16:15:50 +0530

    Episode 06

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,8 @@ import { API_URL } from "../utils/constants";
 
 const Body = () => {
   const [restroList, setRestroList] = useState([]);
+  const [filteredRestro, setFilteredRestro] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -16,30 +18,57 @@ const Body = () => {
     );
 
     const json = await data.json();
-    //console.log(json.data.cards[2].data.data.cards);
+    console.log(json.data.cards[2].data.data.cards);
     setRestroList(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestro(json?.data?.cards[2]?.data?.data?.cards);
   };
-
   return restroList.length === 0 ? (
     <Shimmer />
   ) : (
     <>
       <div className="body">
         <div className="filter">
+          <div className="search">
+            <input
+              type="text"
+              className="search-box"
+              value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                const filteredList = restroList.filter((res) => {
+                  return res.data.name.toLowerCase().includes(searchText);
+                });
+                setFilteredRestro(filteredList);
+              }}
+            >
+              Search
+            </button>
+            <button
+              onClick={() => {
+                setFilteredRestro(restroList);
+              }}
+            >
+              Clear Filters
+            </button>
+          </div>
           <button
             className="filter-btn"
             onClick={() => {
-              const filteredList = restroList.filter(
+              const filteredList1 = restroList.filter(
                 (res) => res.data.avgRating > 4
               );
-              setRestroList(filteredList);
+              setFilteredRestro(filteredList1);
             }}
           >
             Top Rated Restaurents
           </button>
         </div>
         <div className="res-container">
-          {restroList.map((res) => (
+          {filteredRestro.map((res) => (
             <RestaurentCard key={res.data.id} resData={res} />
           ))}
         </div>
