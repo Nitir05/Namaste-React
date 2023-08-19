1eee24ba24e4cf7a7c757157129de2fdc3cb6581
Nitir05 <nrahangdal86@gmail.com>
2023-08-19 18:08:16 +0530

    Testing config done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1>Contact Us at</h1>
-      <h2>Mobile: 7767025270</h2>
-      <h2>Email: support@mail.com</h2>
+    <div className="grid place-items-center">
+      <div className="w-6/12 text-center m-4 border shadow-lg rounded-md">
+        <h1 className="font-bold">Contact Us</h1>
+        <form className="grid col-auto">
+          <input
+            className="border border-black m-2 p-2 rounded-md"
+            type="text"
+            name="Name"
+            id="name"
+            placeholder="Name"
+          />
+          <input
+            className="border border-black m-2 p-2 rounded-md"
+            type="number"
+            name="Phone"
+            id="phone"
+            placeholder="phone"
+          />
+          <input
+            className="border border-black m-2 p-2 rounded-md"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="E-mail"
+          />
+          <button className="border border-black m-2 p-2 rounded-md bg-gray-300">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
