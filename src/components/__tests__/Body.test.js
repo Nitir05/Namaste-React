4afe985b8836ccbfaeec560edd46df1c2b376221
Nitir05 <nrahangdal86@gmail.com>
2023-08-30 09:15:29 +0530

    Updated test code

--- src/components/__tests__/Body.test.js
+++ src/components/__tests__/Body.test.js
@@ -1,2 +1,22 @@
+const { render } = require("@testing-library/react");
+import { act } from "react-dom/test-utils";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/resListMock.json";
+import { BrowserRouter } from "react-router-dom";
 
-it();
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+it("Render the data from API call", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+});
