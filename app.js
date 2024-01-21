1fea192dca9cf58f4fd813aec6658df3ba447901
Nitir05 <nrahangdal86@gmail.com>
2024-01-21 11:05:15 +0530

    Learn: Chapter 03 laying the foundation

--- app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading2 = React.createElement("h2", { className: "heading" }, "This is heading 2");
-const heading = React.createElement("h1", { id: "heading", className: "heading" }, "Namaste React");
-const child = React.createElement("div", { id: "child" }, [heading, heading2]);
-const parent = React.createElement("div", { id: "parent" }, child);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
